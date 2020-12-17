c4b58dfc04747e3169b4ce2fab3a4ed899c4fb56
niuai <1196454838@qq.com>
2020-12-17 19:11:29 +0800

    feat: add custom agent

---
+++ src/Cmb.SkyApm.Agent.AspNetCore/CmbSkyApmHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using Cmb.SkyApm.Agent.AspNetCore;
+using Cmb.SkyApm.Tracing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SkyApm.AspNetCore.Diagnostics;
+using SkyApm.Tracing;
+
+[assembly: HostingStartup(typeof(CmbSkyApmHostingStartup))]
+
+namespace Cmb.SkyApm.Agent.AspNetCore
+{
+    public class CmbSkyApmHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSkyAPM(ext => ext.AddAspNetCoreHosting());
+
+                services.RemoveAll<ICarrierPropagator>();
+                services.AddSingleton<ICarrierPropagator, CmbCarrierPropagator>();
+            });
+        }
+    }
+}
---
+++ src/Cmb.SkyApm.Core/Tracing/CmbCarrierPropagator.cs
@@ -0,0 +1,78 @@
+﻿using SkyApm.Tracing;
+using SkyApm.Tracing.Segments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cmb.SkyApm.Tracing
+{
+    public class CmbCarrierPropagator : ICarrierPropagator
+    {
+        private readonly IEnumerable<ICarrierFormatter> _carrierFormatters;
+        private readonly ISegmentContextFactory _segmentContextFactory;
+
+        public CmbCarrierPropagator(IEnumerable<ICarrierFormatter> carrierFormatters,
+            ISegmentContextFactory segmentContextFactory)
+        {
+            _carrierFormatters = carrierFormatters;
+            _segmentContextFactory = segmentContextFactory;
+        }
+
+        public void Inject(SegmentContext segmentContext, ICarrierHeaderCollection headerCollection)
+        {
+            var reference = segmentContext.References.FirstOrDefault();
+
+            var carrier = new Carrier(segmentContext.TraceId, segmentContext.SegmentId, segmentContext.Span.SpanId,
+                segmentContext.ServiceInstanceId, reference?.EntryServiceInstanceId ?? segmentContext.ServiceInstanceId,
+                segmentContext.ServiceId)
+            {
+                NetworkAddress = segmentContext.Span.Peer,
+                EntryEndpoint = reference?.EntryEndpoint ?? segmentContext.Span.OperationName,
+                ParentEndpoint = segmentContext.Span.OperationName,
+                Sampled = segmentContext.Sampled
+            };
+
+            foreach (var formatter in _carrierFormatters)
+            {
+                if (formatter.Enable)
+                    headerCollection.Add(formatter.Key, formatter.Encode(carrier));
+            }
+        }
+
+        public ICarrier Extract(ICarrierHeaderCollection headerCollection)
+        {
+            ICarrier carrier = NullableCarrier.Instance;
+            if (headerCollection == null)
+            {
+                return carrier;
+            }
+            foreach (var formatter in _carrierFormatters.OrderByDescending(x => x.Key))
+            {
+                if (!formatter.Enable)
+                {
+                    continue;
+                }
+
+                foreach (var header in headerCollection)
+                {
+                    if (formatter.Key == header.Key)
+                    {
+                        carrier = formatter.Decode(header.Value);
+                        if (carrier.HasValue)
+                        {
+                            if (formatter.Key.EndsWith("sw3") && carrier is Carrier c)
+                            {
+                                c.Sampled = true;
+                            }
+
+                            return carrier;
+                        }
+                    }
+                }
+            }
+
+            return carrier;
+        }
+    }
+}
